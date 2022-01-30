c50a35b89e978b442ce3fc91f24fa616a8da6465
Sebastian Rodriguez <60799936+srodzav@users.noreply.github.com>
2022-01-29 19:19:46 -0600

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# .gitignore
-api_key = 'p6IyRfDVrianH4n4SEFfUt7lE'
-api_secret = '7fqws8CgzTdIRfTI6NWEuSrO7gzA7XJRtkpvSf8VDT2LZAbOYE'
-access_token = '1485116050475823105-jN9DglkP3TQJ2rpzjWfliCWae5Jhzm'
-token_secret = '7tGHBj3QQqKbZN6Fw07pAt1Rf49JiBlbNYfGHbO9oT92l'
